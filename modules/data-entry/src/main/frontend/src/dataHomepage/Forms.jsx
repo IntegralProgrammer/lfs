21b5a07df8d117a74a8e82363e3e5c5bc4d75240
Francis Nguyen <OrdinaryNeumann@gmail.com>
2019-11-06 10:30:07 -0500

    LFS-169: As a user, I can fill out a new form Add the new form button, send POST request, route to new form

--- modules/data-entry/src/main/frontend/src/dataHomepage/Forms.jsx
+++ modules/data-entry/src/main/frontend/src/dataHomepage/Forms.jsx
@@ -21,6 +21,7 @@ import LiveTable from "./LiveTable.jsx";
 
 import { Button, Grid, Link, withStyles } from "@material-ui/core";
 import { Card, CardHeader, CardBody } from "MaterialDashboardReact";
+
 import questionnaireStyle from "../questionnaire/QuestionnaireStyle.jsx";
 
 function Forms(props) {
